e8f05c8d46f057dced9ecf2ff9ad70b6415f0b10
Arman <ar_sarsembek@kbtu.kz>
2024-02-03 02:35:36 +0600

    index.html added

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/handlers"
+	"github.com/sarsembek/TSIS1/router"
 	"log"
 	"net/http"
-	"github.com/sarsembek/TSIS1/router"
 )
 
 func main() {
 	r := router.NewRouter()
 
+	// Enable CORS middleware
+	corsHandler := handlers.CORS(
+		handlers.AllowedHeaders([]string{"Content-Type", "Accept", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}), // Adjust this based on your requirements
+	)
+
+	// Use the CORS middleware with your router
+	handler := corsHandler(r)
+
 	port := 8080
 	fmt.Printf("Starting server on :%d...\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
